a2988e5633ee142258feb75440be632881952cd8
Isa-Rocha31415 <isabelrocha31415@gmail.com>
2025-07-31 23:50:52 -0600

    version medio funcional, ventana start medio terminada

--- view/__init__.py
+++ view/__init__.py
@@ -5,3 +5,5 @@ from .layout.ventana_start import Start
 from .layout.ventana_theory import Theory
 from .layout.ventana_tutorial import Tutorial
 from .widgets.boton import boton_menu
+from controller import (mostrar_radial,mostrar_imag_esfericos,mostrar_real_esfericos,
+                                mostrar_cartesian,mostrar_wf_2d,mostrar_wf_3d)
--- view/layout/ventana_start.py
+++ view/layout/ventana_start.py
@@ -6,6 +6,9 @@ from PyQt5.QtGui import QPixmap, QPainter
 from PyQt5.QtCore import Qt
 from .ventana import Ventana
 import os
+from lib.class_electron import plot
+from controller import (mostrar_radial,mostrar_imag_esfericos,mostrar_real_esfericos,
+                                mostrar_cartesian,mostrar_wf_2d,mostrar_wf_3d)
 
 class Start(Ventana):
     def __init__(self):
@@ -16,22 +19,21 @@ class Start(Ventana):
     def init_ui(self):
         parametros = "color: white;font-family: 'Inter';font-weight: bold;font-size: 16px;"
         parametros2 = "color: white;font-family: 'Inter';font-weight: bold;font-size: 25px;"
-        # 1. Define primero la función callback del botón.
-        def mi_callback():
-            print("¡Botón pequeño presionado!")
 
-        def on_number_change(value):
-            print(f"El usuario escribió: {value}")
-
-        # 2. Sigue con el resto de la UI
         layout = QVBoxLayout()
 
-        # Logo
+
+
+        def on_number_change(value):
+            print(f"numero: {value}")
+
+
+        #logo
         png_path = os.path.join("utils","quplotsLogo.png")
         label_logo = QLabel()
         pixmap = QPixmap(png_path)
         pixmap_reescalado = pixmap.scaled(200, 300, Qt.KeepAspectRatio, Qt.SmoothTransformation)
-        # Asignar el QPixmap escalado al label
+        #escalado al label
         label_logo.setPixmap(pixmap_reescalado)
         label_logo.setAlignment( Qt.AlignTop)
         label_logo.setFixedSize(200, 300)  #fijar el tamaño
@@ -42,7 +44,7 @@ class Start(Ventana):
         layout.addWidget(label_numbers, alignment=Qt.AlignHCenter)
 
         
-        # Layout horizontal para cada par
+        #layout horizontal
         campo_n = QHBoxLayout()
         label_n = QLabel("n:")
         label_n.setStyleSheet(parametros2)
@@ -74,45 +76,79 @@ class Start(Ventana):
         fila_nlm.addSpacing(1)
         fila_nlm.addLayout(campo_m)
         layout.addLayout(fila_nlm)
+        callbacks = self.crear_callbacks(number_n, number_l, number_m)
+        self.anadir_widgets(layout, callbacks)
 
+        
+
+    def anadir_widgets(self, layout,callbacks):
+        """
+        Se crean los botones que permiten graficar.
+        Se pasa el objeto de root y los callbacks, que es un diccionario con strings y funciones
+        """
+        #labels con columna
+
+        parametros = "color: white;font-family: 'Inter';font-weight: bold;font-size: 16px;"
+        parametros2 = "color: white;font-family: 'Inter';font-weight: bold;font-size: 25px;"
+        #plot
         label_plot = QLabel("Plot:")
         label_plot.setStyleSheet(parametros2)
         layout.addWidget(label_plot, alignment=Qt.AlignHCenter)
 
-        #---- CADA LABEL+COMBO EN SU COLUMNA, LUEGO EN UNA FILA ----#
-        btn_pequeno = boton_menu("Radial Function", mi_callback, 16, 150, 35,10,10)
-        layout.addWidget(btn_pequeno, alignment=Qt.AlignTop)
+        def on_combo1_changed(index):
+            if index == 0:
+                callbacks['on_real_esfericos']()
+            elif index == 1:
+                callbacks['on_imag']()
+
+        boton_radial = boton_menu("Radial Function", callbacks['on_radial'], 16, 150, 35,10,10)
+        layout.addWidget(boton_radial, alignment=Qt.AlignTop)
 
         grupo1 = QVBoxLayout()
         label1 = QLabel("Spherical Harmonics:")
         label1.setStyleSheet(parametros)
         combo1 = combobox(
-            ["Real", "Imaginary"],
-            on_change=lambda idx: print(f"Combo 1 seleccionó {idx}")
-        )
+            ["Real", "Imaginary"],on_change=on_combo1_changed)
         grupo1.addWidget(label1, alignment=Qt.AlignHCenter)
         grupo1.addWidget(combo1, alignment=Qt.AlignHCenter)
+
+        def on_combo2_changed(index):
+            if index == 0:
+                callbacks['on_wf_2D']()
+            elif index == 1:
+                callbacks['on_wf_3D']()
 
         grupo2 = QVBoxLayout()
         label2 = QLabel("Probability Function:")
         label2.setStyleSheet(parametros)
         combo2 = combobox(
-            ["2D", "3D"],
-            on_change=lambda idx: print(f"Combo 2 seleccionó {idx}")
-        )
+            ["2D", "3D"],on_change=on_combo2_changed)
         grupo2.addWidget(label2, alignment=Qt.AlignHCenter)
         grupo2.addWidget(combo2, alignment=Qt.AlignHCenter)
+
+
+        def on_combo3_changed(index):
+            p=plot()
+            if index == 0:
+                callbacks['on_hibridaciones'](p.plot_sp)
+            elif index == 1:  
+                callbacks['on_hibridaciones'](p.plot_sp2)
+            elif index == 2:  
+                callbacks['on_hibridaciones'](p.plot_sp3)
+            elif index == 3:  
+                callbacks['on_hibridaciones'](p.plot_sp2d)
+            elif index == 4:  
+                callbacks['on_hibridaciones'](p.plot_sp3d)
+            elif index == 5:  
+                callbacks['on_hibridaciones'](p.plot_sp3d2)
 
         grupo3 = QVBoxLayout()
         label3 = QLabel("Hybridization:")
         label3.setStyleSheet(parametros)
         combo3 = combobox(
-            ["sp","sp²","sp³","sp²d","sp³d","sp³d²"],
-            on_change=lambda idx: print(f"Combo 2 seleccionó {idx}")
-        )
+            ["sp","sp²","sp³","sp²d","sp³d","sp³d²"],on_change=on_combo3_changed)
         grupo3.addWidget(label3, alignment=Qt.AlignHCenter)
         grupo3.addWidget(combo3, alignment=Qt.AlignHCenter)
-
 
 
         fila_combos = QHBoxLayout()
@@ -120,7 +156,7 @@ class Start(Ventana):
         fila_combos.addLayout(grupo2)
         fila_combos.addLayout(grupo3)
         fila= QHBoxLayout()
-        fila.addWidget(btn_pequeno, alignment=Qt.AlignVCenter)
+        fila.addWidget(boton_radial, alignment=Qt.AlignVCenter)
         fila.addSpacing(1)
         fila.addLayout(fila_combos)
         layout.addLayout(fila)
@@ -130,18 +166,49 @@ class Start(Ventana):
         label_costume.setStyleSheet(parametros2)
         layout.addWidget(label_costume, alignment=Qt.AlignHCenter)
 
-        # Splitter
-        splitter = QSplitter(Qt.Horizontal)
-        layout.addWidget(splitter)
+        # # Splitter
+        # splitter = QSplitter(Qt.Horizontal)
+        # layout.addWidget(splitter)
 
         
-
-        # Finaliza con el widget central
         central_widget = QWidget()
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
 
-    def obtener_valores_desde_entry(entry_n=None,entry_l=None,entry_m=None):
+    def crear_callbacks(self,entry_n,entry_l,entry_m):
+        """
+        Se definen las funciones de callback que seran retornados en forma de diccionario
+        En la entrada se ingresa los datos de entry_n,entry_l,entry_m.
+        En cada funcion de callback que toma los entry,se validan los elementos 
+        """
+        def on_radial():
+            values=self.obtener_valores_desde_entry(entry_n,entry_l,entry_m)
+            mostrar_radial(values[0],values[1],values[2])
+        def on_real_esfericos():
+            values=self.obtener_valores_desde_entry(entry_n,entry_l,entry_m)
+            mostrar_real_esfericos(values[0],values[1],values[2])
+        def on_imag():
+            values=self.obtener_valores_desde_entry(entry_n,entry_l,entry_m)
+            mostrar_imag_esfericos(values[0],values[1],values[2])
+        def on_wf_2D():
+            values=self.obtener_valores_desde_entry(entry_n,entry_l,entry_m)
+            mostrar_wf_2d(values[0],values[1],values[2])
+        def on_wf_3D():
+            values=self.obtener_valores_desde_entry(entry_n,entry_l,entry_m)
+            mostrar_wf_3d(values[0],values[1],values[2])
+        def on_hibridaciones(func):
+            mostrar_cartesian(func)
+
+        return {
+            'on_radial': on_radial,
+            'on_real_esfericos': on_real_esfericos,
+            'on_imag': on_imag,
+            'on_wf_2D': on_wf_2D,
+            'on_wf_3D': on_wf_3D,
+            'on_hibridaciones': on_hibridaciones
+        }
+
+    def obtener_valores_desde_entry(self,entry_n=None,entry_l=None,entry_m=None):
         """
         Recibe los objetos entry_n,entry_l y entry_m. 
         se guardan los valores recibidos en formato de enteros. 
@@ -150,7 +217,7 @@ class Start(Ventana):
         valores=[]
         for entry in (entry_n,entry_l,entry_m):
             if entry is not None:
-                texto=entry.get().strip()
+                texto=entry.text().strip()
                 if texto=="":
                     valores.append(None)
                 else:
